d5d2d15490150eb3bab16edbd572a454d99823fa
liza9104 <liza9104@icloud.com>
2021-11-14 17:31:14 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/fainaldomit/MainActivity.java
@@ -0,0 +1,71 @@
+package com.cookandroid.fainaldomit;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton mainIcon ;
+    Button myPage_btn, bulletin_btn, rr_btn ;
+    TextView notice_cont, todayMenu_cont ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("메인화면");
+
+        mainIcon = (ImageButton) findViewById(R.id.mainIcon);
+        myPage_btn = (Button) findViewById(R.id.myPage_btn);
+        bulletin_btn = (Button) findViewById(R.id.bulletin_btn);
+        rr_btn = (Button) findViewById(R.id.rr_btn);
+        notice_cont = (TextView) findViewById(R.id.notice_cont);
+        todayMenu_cont = (TextView) findViewById(R.id.todayMenu_cont);
+
+        myPage_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),mypageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        bulletin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getApplicationContext(),boardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+    public void Clickhome(View view) {
+        Toast.makeText(getApplicationContext(),"홈으로 이동",Toast.LENGTH_LONG).show();
+    }
+    public void ClickButton1(View view) {
+        Toast.makeText(getApplicationContext(),"마이페이지 이동",Toast.LENGTH_LONG).show();
+    }
+    public void ClickButton2(View view) {
+        Toast.makeText(getApplicationContext(),"마이페이지 이동",Toast.LENGTH_LONG).show();
+    }
+
+
+}
+
+
+
+
+
+
+
